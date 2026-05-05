bfb65e46baa3629393968b461a6ea4e3e22b365b
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-05 17:17:18 -0400

    feat: Implement REST API endpoints for Times (GET, GET by ID, POST)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using FutApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=futebol.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// GET
+app.MapGet("/times", async (AppDbContext db) =>
+{
+    return await db.Times.ToListAsync();
+});
+
+// GET por Id
+app.MapGet("/times/{id}", async (int id, AppDbContext db) =>
+{
+    var time = await db.Times.FindAsync(id);
+
+    return time != null
+    ? Results.Ok(time)
+    : Results.NotFound("Time não encontrado");
+});
+
+// POST
+app.MapPost("/times", async (AppDbContext db, Time newTime) =>
+{
+    db.Times.Add(newTime);
+    await db.SaveChangesAsync();
+    return Results.Created($"O Time {newTime.Nome} escalado com sucesso!", newTime);
+});
 
 app.Run();
