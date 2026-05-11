bf3af7bc85fa58bbdd6d4c467a0277758a5afe79
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-11 15:29:18 -0400

    feat: Requisição da api e resposta ao front

--- front/script.js
+++ front/script.js
@@ -1,9 +1,32 @@
-const fetchApi = fetch('http://127.0.0.1:5116/times', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
+const listaTimes = document.getElementById('times-lista');
+const apiUrl = 'http://127.0.0.1:5116/times';
+
+const GET = async () => {
+  try {
+    const res = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!res.ok) throw new Error('Erro ao buscar os times!');
+
+    const times = await res.json();
+
+    times.forEach(time => {
+      const li = document.createElement('li');
+      li.innerText = `Nome: ${time.nome}`;
+      listaTimes.appendChild(li);
+    });
+
+  } catch (error) {
+    listaTimes.innerText = `${error.message}`;
   }
-}).then(res => res.json()).then(data => console.log(data));
+}
+
+GET();
+
 
 
 
